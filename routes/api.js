35355202e3ca6bf089ce0adf2785a835541a8e26
Sadan <urang.sadan@gmail.com>
2020-08-01 22:07:01 +0700

    middle

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,8 @@ const { userValidationRules, validate } = require('../middlewares/ValidateReq.js
 /* GET home page. */
 router.get('/package', api.getPackage);
 router.get('/package/:id', api.getPackageById);
-router.put('/package/:id', api.packagePut);
-router.patch('/package/:id', api.packagePut);
+router.put('/package/:id', userValidationRules(), validate, api.packagePut);
+router.patch('/package/:id', userValidationRules(), validate, api.packagePut);
 router.delete('/package/:id', api.packageDelete);
 router.post('/package', userValidationRules(), validate, api.packagePost);
 
